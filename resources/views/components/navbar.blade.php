f19aa8136d9d7fc85ab7524632ecb165ff1a4709
Sofia <sofinazaret6@gmail.com>
2026-04-11 14:46:46 -0300

    actualizacionSofi

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -1,25 +1,40 @@
-<nav class="navbar navbar-expand-lg custom-navbar"> <!-- quitamos navbar-dark y bg-dark -->
-        <div class="container">
-            <!-- CAMBIO: reemplazo de texto por imagen -->
-            <a class="navbar-brand" href="/">
-    <img src="{{ asset('images/LogoRaices.png') }}" alt="Logo" class="img-fluid" style="max-height: 150px;">
-    </a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
-                aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarNav">
-                <ul class="navbar-nav ms-auto">
-                    <li class="nav-item"><a class="nav-link" href="/">Principal</a></li>
-                    <li class="nav-item"><a class="nav-link" href="/quienes-somos">Quiénes Somos</a></li>
-                    <li class="nav-item"><a class="nav-link" href="/catalogo">Catálogo</a></li>
-                    <li class="nav-item"><a class="nav-link" href="#">Consultas</a></li>
-                     <li class="nav-item"><a class="nav-link" href="/contacto">Contacto</a></li>
-                   </ul>
-                    <div class="d-flex ms-lg-3">
-                     <a href="https://facebook.com" class="nav-link px-2 text-white"><i class="fa-brands fa-facebook fa-lg"></i></a>
-                     <a href="mailto:tuemail@ejemplo.com" class="nav-link px-2 text-white"><i class="fa-solid fa-envelope fa-lg"></i></a>
-                   </div>
-            </div>
+<!-- CONTENEDOR SUPERIOR -->
+<div class="barraS">
+<nav class="navbar navbar-expand-lg custom-navbar py-1">
+    <div class="container d-flex justify-content-between align-items-center">
+        <!-- Logo -->
+        <a class="navbar-brand" href="/">
+            <img src="{{ asset('images/logoMiniN.png') }}" alt="Logo" class="img-fluid" style="max-height: 80px;">
+        </a>
+
+        <!-- Redes Sociales (visibles siempre) -->
+        <div class="d-flex">
+            <a href="https://facebook.com" class="nav-link px-2 text-white"><i class="fa-brands fa-facebook fa-lg"></i></a>
+            <a href="mailto:tuemail@ejemplo.com" class="nav-link px-2 text-white"><i class="fa-solid fa-envelope fa-lg"></i></a>
         </div>
-    </nav>
+    </div>
+</nav>
+</div>
+
+<!-- NAV INFERIOR: Menú de Navegación -->
+ <div class="barraI">
+<nav class="navbar navbar-expand-lg custom-navbar py-0" style="border-top: 1px solid rgba(255,255,255,0.1);">
+    <div class="container">
+        <button class="navbar-toggler ms-auto" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+            aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav mx-auto"> <!-- 'mx-auto' para centrar los links -->
+                <li class="nav-item"><a class="nav-link" href="/">Principal</a></li>
+                <li class="nav-item"><a class="nav-link" href="/quienes-somos">Quiénes Somos</a></li>
+                <li class="nav-item"><a class="nav-link" href="/catalogo">Catálogo</a></li>
+                <li class="nav-item"><a class="nav-link" href="#">Consultas</a></li>
+                <li class="nav-item"><a class="nav-link" href="/contacto">Contacto</a></li>
+            </ul>
+        </div>
+    </div>
+</nav>
+</div>
+
