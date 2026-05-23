f5b3726f82cf3f5a02cf065fe2e078b8a24f00f8
Octavio <dorregoocta@gmail.com>
2026-05-22 22:04:25 -0300

    Agrega tablas de productos con stock y categorias

---
+++ app/Http/Controllers/PersonaController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Foundation\Auth\User as Authenticatable;
+
+class Persona extends Authenticatable
+{
+    protected $fillable = [
+        'nombre',
+        'apellido',
+        'telefono',
+        'email',
+        'password',
+        'id_perfil',
+        'estado',
+    ];
+    protected $hidden = [
+        'password',
+    ];
+    protected $casts = [
+        'password' => 'hashed',
+    ];
+
+    // Relación con la tabla de perfiles
+    public function perfil()
+    {
+        return $this->belongsTo(Perfil::class, 'id_perfil');
+    }
+}
