17dca6d66a35d72f54ba670e5a8d90dbaf6be963
Sofia <sofinazaret6@gmail.com>
2026-05-26 09:38:12 -0300

    sapoPepe

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,6 +3,7 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use App\Http\Middleware\RoleMiddleware;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -11,14 +12,11 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        //
+        $middleware->alias([
+            'role' => RoleMiddleware::class,
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
-    })->create();
-    ->withMiddleware(function (Middleware $middleware) {
-    $middleware->alias([
-        'role' => RoleMiddleware::class,
-    ]);
-})
-  
+    })
+    ->create();
