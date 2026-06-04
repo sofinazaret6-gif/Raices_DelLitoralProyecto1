e5f673815b9fa46b09266be637d8f2a3739608a7
Octavio <dorregoocta@gmail.com>
2026-06-04 09:43:51 -0300

    Arreglada la redireccion en PersonaController

---
+++ resources/views/dashboard/listar-ventas.blade.php
@@ -0,0 +1,10 @@
+<x-layout-admin>
+    <x-slot:title>Control de Stock y Visibilidad</x-slot:title>
+
+    <x-slot:barraP>
+        <h4>Control de Inventario</h4>
+     </x-slot:barraP>
+
+
+
+</x-layout-admin>
